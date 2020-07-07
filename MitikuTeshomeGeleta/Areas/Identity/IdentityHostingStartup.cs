602d97e7a53b277a1c088989eb84d971e16542fb
Mitiku Teshome <mitikutesh@gmail.com>
2020-07-07 19:13:14 +0300

    -static page

---
+++ MitikuTeshomeGeleta/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MitikuTeshomeGeleta.Data;
+
+[assembly: HostingStartup(typeof(MitikuTeshomeGeleta.Areas.Identity.IdentityHostingStartup))]
+namespace MitikuTeshomeGeleta.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
