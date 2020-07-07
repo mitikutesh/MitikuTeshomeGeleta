ca67bf699776daaa805a89ed196e80b83bbc4879
Mitiku Teshome <mitikutesh@gmail.com>
2020-07-07 19:17:21 +0300

    -net core integration

---
+++ MitikuTeshomeGeleta/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+$(function () {
+    $("#form-total").steps({
+        headerTag: "h2",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        stepsOrientation: "vertical",
+        autoFocus: true,
+        transitionEffectSpeed: 500,
+        titleTemplate: '<div class="title">#title#</div>',
+        labels: {
+            previous: 'Back Step',
+            next: 'Next',
+            finish: 'Submit',
+            current: ''
+        },
+    })
+});
